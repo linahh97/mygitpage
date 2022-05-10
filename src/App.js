4f1c586de198b374202ec516168d51b9d100b23a
Lina H.H <linah97@hotmail.com>
2022-05-10 23:12:56 +0200

    testar add uppdatera

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {Routes, Route, Link} from 'react-router-dom';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <Routes>
+      <Route path="/mygitpage" element={<Home/>}/>
+      <Route path="/mygitpage/about" element={<About/>}/>
+      <Route path="/mygitpage/privacy" element={<Privacy/>}/>
+    </Routes>
+  </div>
   );
 }
+function Home(){
+  return (
+    <>
+      <nav>
+        <ul className='my-list'>
+          <li>Home</li>
+          <li><Link to="/mygitpage/about">About</Link></li>
+          <li><Link to="/mygitpage/privacy">Privacy</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <h2>Welcome to the homepage</h2>
+        <p>You will get more information later...</p>
+      </main>
+    </>
+  )
+}
 
+function About(){
+  return (
+    <>
+      <nav>
+        <ul className='my-list'>
+          <li><Link to="/mygitpage/home">Home</Link></li>
+          <li>About</li>
+          <li><Link to="/mygitpage/privacy">Privacy</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <h2>Welcome to read more information about me</h2>
+        <p>I am a woman living far north in a country called Sweden, blahblah...</p>
+      </main>
+      <section></section>
+      <footer></footer>
+    </>
+  )
+}
+
+function Privacy(){
+  return (
+    <>
+      <nav>
+        <ul className='my-list'>
+          <li><Link to="/mygitpage/home">Home</Link></li>
+          <li><Link to="/mygitpage/about">About</Link></li>
+          <li>Privacy</li>
+        </ul>
+      </nav>
+      <main>
+        <h2>Privacy</h2>
+        <p>We are very private...</p>
+      </main>
+    </>
+  )
+}
 export default App;
